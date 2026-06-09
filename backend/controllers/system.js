d29230f4fa0bb65d6badbbf0e22e08108be10e32
Umair Anwar <anwarumair801@gmail.com>
2026-06-09 15:06:40 +0500

    Changed VARCHAR limits of the mcq_bank attributes (options, and explanation), and Changed the limit of the TRIBE_MEMBER seats to 120 from 100 in /system/remaining-seats.

--- backend/controllers/system.js
+++ backend/controllers/system.js
@@ -2,7 +2,7 @@ import pool from "../database.js";
 import { handleAsyncError } from "../error.js";
 
 export const getRemainingSeats = handleAsyncError(async (req, res, next) => {
-    const seats = (await pool.query("SELECT 100 - COALESCE(COUNT(student_id), 0)::INT AS remaining_seats FROM students INNER JOIN users ON students.student_id = users.user_id WHERE payment_status='VERIFIED' AND role='TRIBE_MEMBER'")).rows[0].remaining_seats;
+    const seats = (await pool.query("SELECT 120 - COALESCE(COUNT(student_id), 0)::INT AS remaining_seats FROM students INNER JOIN users ON students.student_id = users.user_id WHERE payment_status='VERIFIED' AND role='TRIBE_MEMBER'")).rows[0].remaining_seats;
 
     res.status(200).json({
         status: "success",
